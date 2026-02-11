c666ff7b4e09032773bdba1388874e21788fcbc1
User <User@MacBook-Pro-7.local>
2026-02-11 19:52:59 +1100

    Added comments explaining co-occurrence and co-occurrence gradient functions.

--- R/calculate_co_occurrence3D.R
+++ R/calculate_co_occurrence3D.R
@@ -1,3 +1,36 @@
+#' @title Calculate co-occurrence gradient on 3D spatial data.
+#'
+#' @description This function calculates the co-occurrence on a 3D 
+#'     SpatialExperiment Object. This metric finds the proportion of target
+#'     cells around reference cells relative to the proportion of target cells
+#'     in the SpatialExperiment Object, for each target cell type and for a
+#'     single radius value.
+#'     
+#' @param spe A SpatialExperiment object containing 3D spatial information for 
+#'     the cells. Naming of spatial coordinates MUST be "Cell.X.Position", 
+#'     "Cell.Y.Position", "Cell.Z.Position" for the x-coordinate, y-coordinate 
+#'     and z-coordinate of each cell.
+#' @param reference_cell_type A string specifying the reference cell type.
+#' @param target_cell_types A character vector specifying the target cell types.
+#' @param radius A positive numeric specifying the radius value.
+#' @param feature_colname A string specifying the name of the column in the 
+#'     `colData` slot of the SpatialExperiment object that contains the cell 
+#'     type information. Defaults to "Cell.Type".
+#'
+#' @return A data frame containing the co-occurrence values for each target cell 
+#'     type (columns).
+#'
+#' @examples
+#' result <- calculate_co_occurrence3D(
+#'     spe = SPIAT-3D::simulated_spe,
+#'     reference = "Tumour",
+#'     target = c("Tumour", "Immune"),
+#'     radii = 30
+#'     feature_colname = "Cell.Type
+#' )
+#' 
+#' @export
+
 calculate_co_occurrence3D <- function(spe, 
                                       reference_cell_type, 
                                       target_cell_types, 
--- R/calculate_co_occurrence_gradient3D.R
+++ R/calculate_co_occurrence_gradient3D.R
@@ -1,3 +1,40 @@
+#' @title Calculate co-occurrence gradient on 3D spatial data.
+#'
+#' @description This function calculates the co-occurrence gradient on a 3D 
+#'     SpatialExperiment Object. This metric finds the proportion of target
+#'     cells around reference cells relative to the proportion of target cells
+#'     in the SpatialExperiment Object, for each target cell type, and across 
+#'     all radii values.
+#'     
+#' @param spe A SpatialExperiment object containing 3D spatial information for 
+#'     the cells. Naming of spatial coordinates MUST be "Cell.X.Position", 
+#'     "Cell.Y.Position", "Cell.Z.Position" for the x-coordinate, y-coordinate 
+#'     and z-coordinate of each cell.
+#' @param reference_cell_type A string specifying the reference cell type.
+#' @param target_cell_types A character vector specifying the target cell types.
+#' @param radii A positive, ascending numeric vector specifying the set of 
+#'     radius values used to calculate cells in neighbourhood over a gradient.
+#' @param feature_colname A string specifying the name of the column in the 
+#'     `colData` slot of the SpatialExperiment object that contains the cell 
+#'     type information. Defaults to "Cell.Type".
+#' @param plot_image A logical indicating whether to plot co-occurrence gradient
+#'     as a line graph. Defaults to TRUE.
+#'
+#' @return A data frame containing the co-occurrence values for each target cell 
+#'     type (columns) across each radii (rows).
+#'
+#' @examples
+#' result <- calculate_co_occurrence_gradient3D(
+#'     spe = SPIAT-3D::simulated_spe,
+#'     reference = "Tumour",
+#'     target = c("Tumour", "Immune"),
+#'     radii = seq(20, 100, 10),
+#'     feature_colname = "Cell.Type",
+#'     plot_image = TRUE
+#' )
+#' 
+#' @export
+
 calculate_co_occurrence_gradient3D <- function(spe, 
                                                reference_cell_type, 
                                                target_cell_types, 
