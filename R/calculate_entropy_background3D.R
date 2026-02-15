e1eb94ceb258c9dbc4aa1b05d9e87707eba0e305
User <User@MacBook-Pro-7.local>
2026-02-15 18:24:24 +1100

    Added comments to describe calculate background entropy function.

--- R/calculate_entropy_background3D.R
+++ R/calculate_entropy_background3D.R
@@ -1,3 +1,31 @@
+#' @title Calculate the background entropy of 3D spatial data.
+#'
+#' @description This function calculates the background entropy of a 3D 
+#'     SpatialExperiment object across a subset of cell types chosen by the
+#'     user.
+#'
+#' @param spe A SpatialExperiment object containing 3D spatial information for 
+#'     the cells. Naming of spatial coordinates MUST be "Cell.X.Position", 
+#'     "Cell.Y.Position", "Cell.Z.Position" for the x-coordinate, y-coordinate 
+#'     and z-coordinate of each cell.
+#' @param cell_types_of_interest A character vector specifying the cell types of 
+#'     interest. If NULL, all cell types in the `feature_colname` column will be 
+#'     considered.
+#' @param feature_colname A string specifying the name of the column in the 
+#'     `colData` slot of the SpatialExperiment object that contains the cell 
+#'     type information. Defaults to "Cell.Type".
+#'
+#' @return A numeric representing the background entropy.
+#'
+#' @examples
+#' background_entropy <- calculate_entropy_background3D(
+#'     spe = SPIAT-3D::simulated_spe,
+#'     cell_types_of_interest = c("Tumour", "Immune"),
+#'     feature_colname = "Cell.Type"
+#' )
+#' 
+#' @export
+
 calculate_entropy_background3D <- function(spe,
                                            cell_types_of_interest, 
                                            feature_colname = "Cell.Type") {
